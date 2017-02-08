280f05c092366c986200a5ffba6aeb93c226f8a5
Martin <martin@Martins-MacBook-Pro.local>
2017-02-08 10:25:17 +0100

    Initial Commit

---
+++ TriangulationTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TriangulationTest
+//
+//  Created by Martin on 08/02/2017.
+//  Copyright © 2017 Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
